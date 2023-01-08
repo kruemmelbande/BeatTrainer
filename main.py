68c7ab9a58e7d4b3b2b3c8df02a568be1ebbcff4
kruemmelbande <kai.scheufen@gmail.com>
2023-01-08 17:55:43 +0100

    Added a basic pygame visualizer

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
 print("V0.0.1")
 print("This is just a test, so dont expect anything to work well")
+
 import time, pygame,json
 pygame.init()
 pygame.mixer.init()
-
+screen = pygame.display.set_mode((400, 300))
+board=[[0 for i in range(4)] for i in range(4)]
+def printInColor(text,color):
+    print("\033[38;2;"+str(color[0])+";"+str(color[1])+";"+str(color[2])+"m"+text+"\033[0m",end="")
 def draw(colorNote,last):
-    global indicator
+    global indicator,board
     beatsPerIndicator=20
-    groupwLast=0
     if indicator<beatsPerIndicator:
         ind="["+"-"*indicator+"#"+"-"*(beatsPerIndicator-indicator)+"]"
     else:
         ind="["+"-"*(beatsPerIndicator-(indicator-beatsPerIndicator))+"#"+"-"*(indicator-beatsPerIndicator)+"]"
-    if colorNote["b"]-last["b"]<60/bpm/64:
-        groupwLast=1
+    if colorNote["b"]-last["b"]<60/bpm/256:
         indicator=(indicator+1)%(beatsPerIndicator*2) 
         ind+=" " + ["#-","##","-#"][last["c"]+colorNote["c"]]+" "
+        board[colorNote["x"]][colorNote["y"]]=colorNote["c"]+1
     else:
         ind+=" " + ["#-","-#"][colorNote["c"]]+" "
+        board=[[0 for i in range(4)] for i in range(4)]
+        board[colorNote["x"]][colorNote["y"]]=colorNote["c"]+1
     
-    print(ind,end="\r")
-    return "no u"
+    #draw the board in the console
+        
+    print("\n"*10)
+    for i in range(4):
+        for j in range(4):
+            if board[i][j]==1:
+                printInColor("██",(255,0,0))
+            elif board[i][j]==2:
+                printInColor("██",(0,0,255))
+            
+            else:
+                print("  ",end="")
+        print()
+    #print(ind,end="\r")
+    #draw the board in pygame
+    e=pygame.event.get()
+    for i in e:
+        if i.type==pygame.QUIT:
+            exit()
+    screen.fill((0,0,0))
+    for i in range(4):
+        for j in range(4):
+            if board[i][j]==1:
+                pygame.draw.rect(screen,(255,0,0),(i*100,j*100,100,100))
+            elif board[i][j]==2:
+                pygame.draw.rect(screen,(0,0,255),(i*100,j*100,100,100))
+            
+    pygame.display.flip()
 try:
     with open("beatmap/Info.dat","r") as f:
         info = json.load(f)
@@ -53,7 +84,7 @@ if devmode:
 #load the music
 pygame.mixer.music.load("beatmap/"+musicfile)
 #set the volume
-pygame.mixer.music.set_volume(0.1)
+pygame.mixer.music.set_volume(0.05)
 pygame.mixer.music.play()
 #start the map
 starttime=time.time()
@@ -68,5 +99,5 @@ for i in beatmap["colorNotes"]:
     last=i
 while pygame.mixer.music.get_busy():
     time.sleep(0.1)
-print("Done!")
+print("\nDone!")
     
