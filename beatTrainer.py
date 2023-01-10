0def923541623b8247a99289f87452d4bfc5c517
kruemmelbande <kai.scheufen@gmail.com>
2023-01-10 19:40:21 +0100

    Improvements the the gui of beatTrainer

--- beatTrainer.py
+++ beatTrainer.py
@@ -1,48 +1,71 @@
 import tkinter as tk
-from tkinter import filedialog
-from beatTrainerCore import loadbeatmap, convertLoaded, getLoadedDifficulties, clearLoaded
+from tkinter import filedialog, PhotoImage
+from beatTrainerCore import loadbeatmap, convertLoaded, getLoadedDifficulties, clearLoaded, getBeatmapName, getThumbnail
 
 def on_load_button_click():
+    global img_label
     filepath = filedialog.askdirectory()
-    loadbeatmap(filepath)
-    difficulties = getLoadedDifficulties()
-    difficulty_var.set(difficulties[0])
-    difficulty_menu['menu'].delete(0, 'end')
-    for difficulty in difficulties:
-        difficulty_menu['menu'].add_command(label=difficulty, command=lambda val=difficulty: difficulty_var.set(val))
+    load_result = loadbeatmap(filepath)
+    
+    if load_result == 0:
+        name = getBeatmapName()
+        beatmap_label.config(text=name)
+        difficulties = getLoadedDifficulties()
+        difficulty_var.set(difficulties[0])
+        difficulty_menu['menu'].delete(0, 'end')
+        for difficulty in difficulties:
+            difficulty_menu['menu'].add_command(label=difficulty, command=tk._setit(difficulty_var, difficulty))
 
         convert_button['state'] = 'normal'
         clear_button['state'] = 'normal'
+        img = getThumbnail()
+        #show the image
+        img_label = tk.Label(root, image=img)
+        img_label.image = img
+        img_label.grid(row=0, column=1, sticky='W')
+
+
 
 def on_convert_button_click():
+    beatmap_label.config(text="Converting...")
+    root.update()
     difficulty = difficulty_var.get()
     save_location = filedialog.askdirectory()
-    convertLoaded(difficulty, save_location)
+    conversion_result = convertLoaded(difficulty, save_location)
+    if conversion_result == 0:
+        beatmap_label.config(text="Conversion Successful!")
+    else:
+        beatmap_label.config(text="Conversion Failed!")
 
 def on_clear_button_click():
     clearLoaded()
     difficulty_var.set('')
-    difficulty_menu['values'] = []
+    difficulty_menu['menu'].delete(0, 'end')
     convert_button['state'] = 'disable'
     clear_button['state'] = 'disable'
+    beatmap_label.config(text="No beatmap loaded")
 
 root = tk.Tk()
-root.title("Beat3 Training Map Converter")
-root.geometry("800x600")
-
+root.title("BeatTrainer")
+root.geometry("300x300")
+root.columnconfigure(0, minsize=50, pad=10)
+root.rowconfigure(0, minsize=50, pad=10)
 load_button = tk.Button(root, text="Load Beatmap", command=on_load_button_click)
-load_button.pack()
+load_button.grid(row=0, column=0, sticky='W')
+
+beatmap_label = tk.Label(root, text="No beatmap loaded")
+beatmap_label.grid(row=1, column=1, sticky='W')
 
 difficulty_var = tk.StringVar(value='')
 difficulty_menu = tk.OptionMenu(root, difficulty_var, [])
-difficulty_menu.pack()
+difficulty_menu.grid(row=1, column=0, sticky='W')
 
 convert_button = tk.Button(root, text="Convert", command=on_convert_button_click)
-convert_button.pack()
+convert_button.grid(row=3, column=0, sticky='W')
 convert_button['state'] = 'disable'
 
 clear_button = tk.Button(root, text="Clear", command=on_clear_button_click)
-clear_button.pack()
+clear_button.grid(row=4, column=0,sticky='W')
 clear_button['state'] = 'disable'
 
 root.mainloop()
--- beatTrainerCore.py
+++ beatTrainerCore.py
@@ -1,4 +1,5 @@
 import json,os
+from PIL import Image, ImageTk
 currentBeatmap={}
 beemaps=[]
 song=None
@@ -8,18 +9,38 @@ def makeCopy(list):
     for i in list:
         newList[i]=list[i]
     return newList
-def loadbeatmap(filepath):
-    global beemaps,info,song,cover
-    beemaps={}
-    with open(filepath+"/Info.dat","r") as f:
-        info=json.load(f)
-    #print(json.dumps(info,indent=4))
-    for i in info["_difficultyBeatmapSets"][0]["_difficultyBeatmaps"]:
-        with open(filepath+"/"+i["_beatmapFilename"],"r") as f:
-            beemaps[str(i["_difficulty"])]=json.load(f)
-    song=open(filepath+"/"+info["_songFilename"],"rb")
-    cover=open(filepath+"/"+info["_coverImageFilename"],"rb")
 
+def getThumbnail():
+    global thumnail
+    try:
+        im = thumbnail.copy()
+        im = im.resize((100,100), Image.ANTIALIAS)
+        #im.show() <- shows the image correctly
+        return ImageTk.PhotoImage(im)
+    except:
+        return None
+def getBeatmapName():
+    global info
+    try:
+        return str(info["_songName"])
+    except:
+        return "No beatmap loaded"
+def loadbeatmap(filepath):
+    try:
+        global beemaps,info,song,cover, thumbnail
+        beemaps={}
+        with open(filepath+"/Info.dat","r") as f:
+            info=json.load(f)
+        #print(json.dumps(info,indent=4))
+        for i in info["_difficultyBeatmapSets"][0]["_difficultyBeatmaps"]:
+            with open(filepath+"/"+i["_beatmapFilename"],"r") as f:
+                beemaps[str(i["_difficulty"])]=json.load(f)
+        thumbnail=Image.open(filepath+"/"+info["_coverImageFilename"])
+        song=open(filepath+"/"+info["_songFilename"],"rb")
+        cover=open(filepath+"/"+info["_coverImageFilename"],"rb")
+        return 0
+    except:
+        return 1
 def getLoadedDifficulties():
     return [i for i in beemaps]
 
@@ -31,73 +52,79 @@ def clearLoaded():
     cover=None
 
 def convertLoaded(difficulty,saveLocation):
-    global beemaps,currentBeatmap,info
-    currentBeatmap=beemaps[difficulty]
     try:
-        v=currentBeatmap["version"]
-    except:
+        global beemaps,currentBeatmap,info
+        currentBeatmap=beemaps[difficulty]
         try:
-            v=currentBeatmap["_version"]
+            v=currentBeatmap["version"]
         except:
-            print("Couldnt determine version of map (trying to continue anyway)")
-            v="3.0.0"
-    if v.startswith("2."):
-        from v2tov3 import convert
-        currentBeatmap=convert(currentBeatmap)
-    lefts=[]
-    rights=[]
-    easy={}
-    normal={}
-    #print(str(json.dumps(currentBeatmap,indent=4)[:1000]))
-    for i in currentBeatmap["colorNotes"]:
-        if i["c"] == 0:
-            lefts.append(i)
-        else:
-            rights.append(i)
-    
-    easy=makeCopy(currentBeatmap)
-    normal=makeCopy(currentBeatmap)
-    easy["colorNotes"]=lefts
-    normal["colorNotes"]=rights
-    newInfo=makeCopy(info)
-    ogInfo={}
-    for i in info["_difficultyBeatmapSets"][0]["_difficultyBeatmaps"]:
-        if i["_difficulty"] == difficulty:
-            ogInfo=i
-            break
-    easyLevel=makeCopy(ogInfo)
-    easyLevel["_difficulty"]="Easy"
-    easyLevel["_difficultyRank"]=1
-    easyLevel["_beatmapFilename"]="StandartEasy.dat"
-    normalLevel=makeCopy(ogInfo)
-    normalLevel["_difficulty"]="Normal"
-    normalLevel["_difficultyRank"]=3
-    normalLevel["_beatmapFilename"]="StandartNormal.dat"
-    newInfo["_difficultyBeatmapSets"]=[{}]
-    newInfo["_difficultyBeatmapSets"][0]["_beatmapCharacteristicName"]="Standard"
-    newInfo["_difficultyBeatmapSets"][0]["_difficultyBeatmaps"]=[easyLevel,normalLevel]
-    newInfo["_songSubName"]="(training edition)"
-    try:
-        #delete all existing files
-        os.remove(saveLocation+"/Info.dat")
-        os.remove(saveLocation+"/StandartEasy.dat")
-        os.remove(saveLocation+"/StandartNormal.dat")
-        os.remove(saveLocation+"/"+newInfo["_songFilename"])
-        os.remove(saveLocation+"/"+newInfo["_coverImageFilename"])
+            try:
+                v=currentBeatmap["_version"]
+            except:
+                print("Couldnt determine version of map (trying to continue anyway)")
+                v="3.0.0"
+        if v.startswith("2."):
+            from v2tov3 import convert
+            currentBeatmap=convert(currentBeatmap)
+        lefts=[]
+        rights=[]
+        easy={}
+        normal={}
+        #print(str(json.dumps(currentBeatmap,indent=4)[:1000]))
+        for i in currentBeatmap["colorNotes"]:
+            if i["c"] == 0:
+                lefts.append(i)
+            else:
+                rights.append(i)
+        
+        easy=makeCopy(currentBeatmap)
+        normal=makeCopy(currentBeatmap)
+        easy["colorNotes"]=lefts
+        normal["colorNotes"]=rights
+        newInfo=makeCopy(info)
+        ogInfo={}
+        for i in info["_difficultyBeatmapSets"][0]["_difficultyBeatmaps"]:
+            if i["_difficulty"] == difficulty:
+                ogInfo=i
+                break
+        easyLevel=makeCopy(ogInfo)
+        easyLevel["_difficulty"]="Easy"
+        easyLevel["_difficultyRank"]=1
+        easyLevel["_beatmapFilename"]="StandartEasy.dat"
+        normalLevel=makeCopy(ogInfo)
+        normalLevel["_difficulty"]="Normal"
+        normalLevel["_difficultyRank"]=3
+        normalLevel["_beatmapFilename"]="StandartNormal.dat"
+        newInfo["_difficultyBeatmapSets"]=[{}]
+        newInfo["_difficultyBeatmapSets"][0]["_beatmapCharacteristicName"]="Standard"
+        newInfo["_difficultyBeatmapSets"][0]["_difficultyBeatmaps"]=[easyLevel,normalLevel]
+        newInfo["_songSubName"]="(training edition)"
+        try:
+            #delete all existing files
+            os.remove(saveLocation+"/Info.dat")
+            os.remove(saveLocation+"/StandartEasy.dat")
+            os.remove(saveLocation+"/StandartNormal.dat")
+            os.remove(saveLocation+"/"+newInfo["_songFilename"])
+            os.remove(saveLocation+"/"+newInfo["_coverImageFilename"])
+        except:
+            pass
+        try:
+            os.mkdir(saveLocation)
+        except:
+            pass
+        with open(saveLocation+"/Info.dat","w") as f:
+            json.dump(newInfo,f,indent=2)
+        with open(saveLocation+"/StandartEasy.dat","w") as f:
+            json.dump(easy,f)
+        with open(saveLocation+"/StandartNormal.dat","w") as f:
+            json.dump(normal,f)
+        with open(saveLocation+"/"+newInfo["_songFilename"], "wb") as f:
+            f.write(song.read())
+        with open(saveLocation+"/"+newInfo["_coverImageFilename"], "wb") as f:
+            f.write(cover.read())
+        print("Done!")
+        return 0
     except:
-        pass
-    try:
-        os.mkdir(saveLocation)
-    except:
-        pass
-    with open(saveLocation+"/Info.dat","w") as f:
-        json.dump(newInfo,f,indent=2)
-    with open(saveLocation+"/StandartEasy.dat","w") as f:
-        json.dump(easy,f)
-    with open(saveLocation+"/StandartNormal.dat","w") as f:
-        json.dump(normal,f)
-    with open(saveLocation+"/"+newInfo["_songFilename"], "wb") as f:
-        f.write(song.read())
-    with open(saveLocation+"/"+newInfo["_coverImageFilename"], "wb") as f:
-        f.write(cover.read())
-    print("Done!")
+        return 1
+if __name__ == "__main__":
+    print("This is a library, not a program. Run beatTrainer.py instead.")
